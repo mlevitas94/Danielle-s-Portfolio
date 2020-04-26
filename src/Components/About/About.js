62dec4fc4d288853f7b9024a1c8f0d69a7143cd7
mlevitas94 <levitasmc@gmail.com>
2020-04-26 16:54:12 -0400

    progress

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -8,10 +8,11 @@ const About = () => {
             <div className='aboutCont'>
                 <div className='middleCard'>
                     <div className='imgSide'>
+                        <h1>About Me</h1>
                         <img src={Portrait} alt='portrait'/>
                     </div>
                     <div className='contentSide'>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At lectus urna duis convallis convallis. Faucibus nisl tincidunt eget nullam. Quisque id diam vel quam. Leo a diam sollicitudin tempor id eu nisl. Aenean et tortor at risus. Eu facilisis sed odio morbi quis commodo odio aenean sed. Sem integer vitae justo eget magna fermentum iaculis. Tincidunt arcu non sodales neque sodales ut etiam. Ultrices neque ornare aenean euismod elementum nisi quis eleifend quam. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus. Volutpat consequat mauris nunc congue nisi vitae. Suspendisse ultrices gravida dictum fusce ut placerat orci nulla pellentesque. Adipiscing commodo elit at imperdiet dui accumsan.</p>
+                        <div className='content'><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At lectus urna duis convallis convallis. Faucibus nisl tincidunt eget nullam. Quisque id diam vel quam. Leo a diam sollicitudin tempor id eu nisl. Aenean et tortor at risus. Eu facilisis sed odio morbi quis commodo odio aenean sed. Sem integer vitae justo eget magna fermentum iaculis. Tincidunt arcu non sodales neque sodales ut etiam. Ultrices neque ornare aenean euismod elementum nisi quis eleifend quam. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus. Volutpat consequat mauris nunc congue nisi vitae. Suspendisse ultrices gravida dictum fusce ut placerat orci nulla pellentesque. Adipiscing commodo elit at imperdiet dui accumsan.</p></div>
                     </div>
                 </div>
             </div>
