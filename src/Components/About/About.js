17c53bfa145bddc3e9e98e54cf3b51d66aa39f08
mlevitas94 <levitasmc@gmail.com>
2020-05-04 15:05:16 -0400

    progress

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './About.scss'
-import Portrait from './assets/portrait.jpg'
 import PhotoBy from '../../assets/Components/PhotoBy/PhotoBy'
 
 const About = () => {
@@ -10,13 +9,22 @@ const About = () => {
                 <div className='middleCard'>
                     <div className='imgSide'>
                         <h1>About Me</h1>
-                        <img src={Portrait} alt='portrait'/>
+                        <div className='image'></div>
                     </div>
                     <div className='contentSide'>
                         <div className='content'>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At lectus urna duis convallis convallis. Faucibus nisl tincidunt eget nullam. Quisque id diam vel quam. Leo a diam sollicitudin tempor id eu nisl. Aenean et tortor at risus. Eu facilisis sed odio morbi quis commodo odio aenean sed. Sem integer vitae justo eget magna fermentum iaculis. Tincidunt arcu non sodales neque sodales ut etiam. Ultrices neque ornare aenean euismod elementum nisi quis eleifend quam. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus. Volutpat consequat mauris nunc congue nisi vitae. Suspendisse ultrices gravida dictum fusce ut placerat orci nulla pellentesque. Adipiscing commodo elit at imperdiet dui accumsan.</p>
+                            <p>
+                                Hi! My name is Danielle, and I am a sucker for words: Big words, short words, simple words, and even those long spelling bee words that no one ever knows. Words, grammar, and writing — that’s my thing.
+                                <br />
+                                <br />
+
+                                I like to think of myself as a millennial copywriter and editor in a sea of other millennial copywriters and editors searching for their niche. I've written about historic downtown NYC, basketball, pop culture, and topics in between; I'm just trying to find what I connect with.
+                                <br />
+                                <br />
+
+                                Also, here are some fun facts: I dance on a competitive team, I work three jobs (the hustle is real), I have a fake tooth, and my dream is to find a career that allows all of my passions to coincide in harmony. Also, you will usually find me sipping on bubble tea or eating one of my 15 snacks of the day — it’s a lifestyle choice.</p>
                         </div>
-                        <PhotoBy/>
+                        <PhotoBy />
                     </div>
                 </div>
             </div>
--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -9,24 +9,29 @@ const Contact = () => {
                     <div className='contentSide'>
                         <div className='content'>
                             <h1>Contact Me</h1>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis eget nunc lobortis mattis. Enim nunc faucibus a pellentesque sit amet porttitor eget. Vitae proin sagittis nisl rhoncus mattis rhoncus urna neque viverra. Egestas tellus rutrum tellus pellentesque eu tincidunt.</p>
-                            <ul>
-                                <li>Email : Email@mail.com</li>
-                                <li>Linkedin : www.linkedin.com/myprofile</li>
-                                <li>Instagram : www.instagram.com/myprofile</li>
-                                <li>Twitter : www.twitter.com/myprofile</li>
-                            </ul>
+                            <div>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis eget nunc lobortis mattis. Enim nunc faucibus a pellentesque sit amet porttitor eget. Vitae proin sagittis nisl rhoncus mattis rhoncus urna neque viverra. Egestas tellus rutrum tellus pellentesque eu tincidunt.</p>
+                                <ul>
+                                    <li>Email : Email@mail.com</li>
+                                    <li>Linkedin : www.linkedin.com/myprofile</li>
+                                    <li>Instagram : www.instagram.com/myprofile</li>
+                                    <li>Twitter : www.twitter.com/myprofile</li>
+                                </ul>
+                            </div>
                             <form>
-                                <div>
-                                    <label>Name</label>
-                                    <input />
-                                </div>
-                                <div>
-                                    <label>Email</label>
-                                    <input />
+                                <div className='topForm'>
+                                    <div>
+                                        <label>Name</label>
+                                        <input />
+                                    </div>
+                                    <div>
+                                        <label>Email</label>
+                                        <input />
+                                    </div>
+
                                 </div>
                                 <label>Message</label>
-                                <textarea></textarea>
+                                <textarea rows='5'></textarea>
                             </form>
 
                         </div>
