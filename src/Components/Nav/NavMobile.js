c80c4bffd562d557097bc5ffac1ebbdbcb9964a0
mlevitas94 <levitasmc@gmail.com>
2020-07-03 17:10:47 -0400

    mobile nav working

--- src/Components/Nav/NavMobile.js
+++ src/Components/Nav/NavMobile.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Link } from 'react-router-dom'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import './Nav.scss'
 
 const NavMobile = () => {
     return (
         <div className='navMobile'>
-            <div className='buttonCont'>
-                <FontAwesomeIcon icon={faBars} />
-
+            <div className='buttonCont' onMouseLeave={(e) => {
+                document.querySelector('.buttonCont').classList.remove('extndToggle')
+            }}
+            onMouseEnter={(e) => {
+                document.querySelector('.buttonCont').classList.remove('extndToggle')
+            }}>
+                <FontAwesomeIcon icon={faBars} 
+                onClick={(e) => {
+                    document.querySelector('.buttonCont').classList.toggle('extndToggle')
+                }}/>
+                <div className='linkExt'>
+                    <ul>
+                        <li><Link to='/about'>About</Link></li>
+                        <li><Link to='/projects'>Projects</Link></li>
+                        <li><Link to='/contact'>Contact</Link></li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
