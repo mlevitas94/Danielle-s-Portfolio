e71530b208b60c664128eb435c96b9c694ced5a2
mlevitas94 <levitasmc@gmail.com>
2020-07-10 22:07:04 -0400

    back button for selected project

--- src/Components/Projects/Project.js
+++ src/Components/Projects/Project.js
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react'
 import './Projects.scss'
 import testImg from '../../assets/park.jpg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowAltCircleLeft } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft} from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
 
 const Project = () => {
@@ -14,6 +14,9 @@ const Project = () => {
     }, [])
     return (
         <div className='selectedProject'>
+            <div className='backBtn'>
+                <Link to='/projects'><FontAwesomeIcon icon={faArrowLeft}/></Link>
+            </div>
             
             <h1>Project Name that could be long</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor vitae purus faucibus ornare. Rhoncus mattis rhoncus urna neque viverra. Mauris ultrices eros in cursus turpis massa tincidunt dui. Mattis vulputate enim nulla aliquet porttitor. Id eu nisl nunc mi ipsum faucibus vitae. Cras ornare arcu dui vivamus arcu felis bibendum. Urna et pharetra pharetra massa massa ultricies mi. Ut placerat orci nulla pellentesque dignissim enim. Lacinia quis vel eros donec ac odio tempor orci.</p>
